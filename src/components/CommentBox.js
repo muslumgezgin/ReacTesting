f8fa0a16fbc4ce99affdce999a61d0d89f676454
muslumgezgin <muslum_gezgin>
2019-03-17 14:13:11 +0100

    introduce afterEach to clean up test wrapper

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+  state = { comment: "" };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // TODO - call an action creator
+    //adn save the comment
+
+    this.setState({ comment: "" });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
