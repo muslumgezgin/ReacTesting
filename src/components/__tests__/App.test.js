9595f3690851ec6f8f4ef7d4e37a138b7e1b44b0
muslumgezgin <muslum_gezgin>
2019-03-17 12:54:50 +0100

    add basic test to see comment box is inside the app.js

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "../App";
 
-it("renders without crashing", () => {
+it("shows a comment box", () => {
   const div = document.createElement("div");
+
   ReactDOM.render(<App />, div);
 
-  expect(div.innerHTML).toContain("Hi there");
+  // expect(div.innerHTML).toContain("Comment Box");
 
-  
   ReactDOM.unmountComponentAtNode(div);
+  // clean up
 });
