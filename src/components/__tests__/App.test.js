59a891179bc95a1c278754456e7427f0a2e33de0
muslumgezgin <muslum_gezgin>
2019-03-17 13:26:45 +0100

    introduced ezyme testing to react app and test it app.js shows one comment ox inside it self

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "../App";
+import CommentBox from "../CommentBox";
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
-
-  ReactDOM.render(<App />, div);
-
-  // expect(div.innerHTML).toContain("Comment Box");
-
-  ReactDOM.unmountComponentAtNode(div);
-  // clean up
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
