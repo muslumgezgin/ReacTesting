4228886ef1e432deeb1dcbaa036c76a5992bad3c
muslum <muslum_gezgin@hotmail.com>
2019-04-18 17:03:44 +0200

    finalise server side for login and registiration securly

--- server/index.js
+++ server/index.js
@@ -3,12 +3,19 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
-const router=require('./router')
+const router=require('./router');
+const mongoose =require('mongoose')
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+
+mongoose.connect('mongodb://localhost:27017/auth');
 //app setup
 
 app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*' }))
+app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 //server setup
@@ -16,4 +23,4 @@ router(app);
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
-console.log('server listening on : ' + port)
+console.log('server listening on : ' + port);
